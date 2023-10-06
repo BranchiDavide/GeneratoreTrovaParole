9aee0eff409444335fe22eb6381f0982d1c2317b
BranchiDavide <davide.branchi@samtrevano.ch>
2023-10-06 11:34:50 +0200

    Aggiornamento doc e input-manager

--- 5_Applicativo/GeneratoreTrovaParole/public/js/input-manager.js
+++ 5_Applicativo/GeneratoreTrovaParole/public/js/input-manager.js
@@ -1,28 +1,62 @@
 let titleInput = document.getElementById("titleInput");
 let wordsInput = document.getElementsByClassName("word-input");
 let allInputs = Array.from(wordsInput);
+let insertedCharsCount = document.getElementById("insertedCharsCount");
 allInputs.push(titleInput);
+/**
+ * Funzione per cambiare il bordo dell'input in base alla
+ * presenza di un testo o meno
+ * @param input input a cui cambiare il bordo
+ */
 function changeBorderInput(input){
     if(input.value.trim() != ""){
-        input.style.border = "none";
+        if(input.value.trim().length > 15){
+            if(input.id != "titleInput"){
+                input.style.border = "1px solid red";
+                input.setAttribute("title", "La parola non può superare 15 caratteri");
+            }
+        }else{
+            input.style.border = "none";
+            input.removeAttribute("title");
+        }
     }else{
         input.style.border = "1px dashed black";
     }
 }
+let charCounter = 0;
+/**
+ * Funzione per aggiornare il conteggio dei caratteri inseriti
+ */
+function updateCharsCount(){
+    charCounter = 0
+    for(let input of wordsInput){
+        charCounter += input.value.trim().length;
+    }
+    insertedCharsCount.innerText = charCounter;
+    if(charCounter < 140 || charCounter > 147){
+        insertedCharsCount.style.color = "red";
+    }else{
+        insertedCharsCount.style.color = "green";
+    }
+}
+/*
+Ciclo per eseguire ogni volta le funzioni changeBorderInput() e updateCharsCount()
+quando viene premuto un tasto su un input
+*/
 for(let input of allInputs){
     input.addEventListener("keyup", () => {
         changeBorderInput(input);
+        updateCharsCount();
     });
 }
-let lastAdded = 0;
-let allAdded = false;
 for(let i = 0; i < wordsInput.length; i++){
+    //Evento per gestire l'aggiunta o la rimozione delle parole dagli input
     wordsInput[i].addEventListener("keyup", () =>{
-        if(wordsInput[i].value.trim() != ""){
+        if(wordsInput[i].value.trim() != ""){ //Aggiunta di un nuovo input
             if(i != wordsInput.length - 1){
                 wordsInput[i + 1].style.visibility = "visible";
             }
-        }else{
+        }else{ //Rimozione di un input e spostamento delle parole
             if(i != wordsInput.length - 1){
                 if(wordsInput[i + 1].value.trim() == ""){
                     wordsInput[i + 1].style.visibility = "hidden";
@@ -30,12 +64,14 @@ for(let i = 0; i < wordsInput.length; i++){
                 }else{
                     let current = i;
                     let j = current + 1;
+                    //Ciclo per spostare le parole quando ne viene rimossa una
                     while(wordsInput[j].value.trim() != ""){
                         wordsInput[current].value = wordsInput[j].value;
                         if(j + 1 != wordsInput.length){
                             current++;
                             j++;
                         }else{
+                             //Quando viene raggiunto l'ultimo input in ciclo viene interrotto
                             break;
                         }
                     }
